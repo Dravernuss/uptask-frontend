33c8beb8c74500abb9c1bd8291c39e48251f10d6
Dravernuss <esteban16.rodas@gmail.com>
2025-03-02 18:25:15 -0500

    Recover password, auth and success login

--- src/router.tsx
+++ src/router.tsx
@@ -7,8 +7,10 @@ import ProjectDetailsView from "@/views/projects/ProjectDetailsView";
 import LoginView from "@/views/auth/LoginView";
 import AuthLayout from "@/layouts/AuthLayout";
 import RegisterView from "@/views/auth/RegisterView";
-import ConfirmAccountView from "./views/auth/ConfirmAccountView";
-import RequestNewCodeView from "./views/auth/RequestNewCodeView";
+import ConfirmAccountView from "@/views/auth/ConfirmAccountView";
+import RequestNewCodeView from "@/views/auth/RequestNewCodeView";
+import ForgotPasswordView from "@/views/auth/ForgotPasswordView";
+import NewPasswordView from "./views/auth/NewPasswordView";
 
 export default function Router() {
   return (
@@ -31,6 +33,11 @@ export default function Router() {
             element={<ConfirmAccountView />}
           />
           <Route path="/auth/request-code" element={<RequestNewCodeView />} />
+          <Route
+            path="/auth/forgot-password"
+            element={<ForgotPasswordView />}
+          />
+          <Route path="/auth/new-password" element={<NewPasswordView />} />
         </Route>
       </Routes>
     </BrowserRouter>
